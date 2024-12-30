2b35cfaad06e5f6a59e028b981da7e3fa8b6dbb5
Searica <143636061+searica@users.noreply.github.com>
2024-12-30 10:32:20 -0800

    Use coroutine

--- Core/TradeRouteMap.cs
+++ Core/TradeRouteMap.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ShowMeTheGoods.Extensions;
+using Logging;
 using UnityEngine;
 
 
@@ -17,34 +17,64 @@ internal class TradeRouteMap : MonoBehaviour
     /// <summary>
     ///  maybe make this static to prevent just spamming multiple maps
     /// </summary>
-    private DateTime lastReadTime;
-    //private const long cooldown = 5 * TimeSpan.TicksPerMinute;
-    private const long cooldown = 5 * TimeSpan.TicksPerSecond;
+    private const float PinDuration = 10f; // seconds
+    private Minimap.PinData TraderPinData   ;
+    private const float DefaultRadius = 100f;
+    private const string DefaultPinName = "Trader?";
 
     public void Awake()
     {
-        lastReadTime = DateTime.MinValue;
     }
 
-    public bool FindClosestTrader(out ZoneSystem.LocationInstance closest)
+    public void PinClosestUndiscoveredTraderLocation()
+    {
+        if (!CanReadMap()) { return; }
+        if (this.FindClosestTraderLocation(out ZoneSystem.LocationInstance locationInstance))
+        {
+            Log.LogInfo($"Closest Trader Location: {locationInstance.m_location.m_prefabName}");
+        }
+
+        if (!Player.m_localPlayer || !Minimap.instance || !InventoryGui.instance)
+        {
+            return;
+        }
+
+        // Create Pin and modify size
+        Vector3 pinPos = locationInstance.m_position;  // want to add random noise to this
+        TraderPinData = Minimap.instance.AddPin(pinPos, Minimap.PinType.EventArea, DefaultPinName, save: false, isChecked: false);
+        TraderPinData.m_worldSize = DefaultRadius; // want to add random noise to this
+
+        // Show Pin on the map
+        InventoryGui.instance.Hide();  // force close to prevent getting locked from input
+        Sprite sprite = Minimap.instance.GetSprite(Minimap.PinType.EventArea);
+        Player.m_localPlayer.Message(MessageHud.MessageType.TopLeft, $"$msg_pin_added: {DefaultPinName}", 0, sprite);
+        Minimap.instance.ShowPointOnMap(pinPos);
+
+        if (!this.gameObject.activeInHierarchy)
+        {
+            this.gameObject.SetActive(true);
+        }
+        Log.LogInfo($"GamObject active: {this.gameObject.activeInHierarchy}");
+        Log.LogInfo($"TraderRouteMap active: {this.isActiveAndEnabled}");
+
+        // Remove Pin after PinDuration elapses
+        StartCoroutine(RemovePinDataAfter(PinDuration));
+    }
+
+    internal bool FindClosestTraderLocation(out ZoneSystem.LocationInstance closest)
     {
         closest = default;
         bool result = false;
-
-        if (!CanReadMap())
-        {
-            return result;
-        }
-    
+   
         Vector3 point = Player.m_localPlayer.transform.position;
         float minDistance = float.MaxValue;
-        foreach (ZoneSystem.LocationInstance value in ZoneSystem.instance.m_locationInstances.Values)
+        foreach (ZoneSystem.LocationInstance locationInstance in ZoneSystem.instance.m_locationInstances.Values)
         {
-            float distance = Vector3.Distance(value.m_position, point);
-            if (value.m_location.IsTraderLocation() && distance < minDistance)
+            float distance = Vector3.Distance(locationInstance.m_position, point);
+            if (TraderLocatonManager.IsTraderLocation(locationInstance.m_location) && distance < minDistance)
             {
                 minDistance = distance;
-                closest = value;
+                closest = locationInstance;
                 result = true;
             }
         }
@@ -57,18 +87,32 @@ internal class TradeRouteMap : MonoBehaviour
     /// <returns></returns>
     private bool CanReadMap()
     {
-        DateTime now = DateTime.Now;
-        long deltaTime = now.Ticks - lastReadTime.Ticks;
-        if (deltaTime < cooldown)
+        if (TraderPinData is null)
         {
-            if (Player.m_localPlayer)
-            {
-                Player.m_localPlayer.Message(MessageHud.MessageType.Center, "Cannot read trade route map again yet.");
-            }
-            return false;
+            return true;
         }
-        lastReadTime = now;
-        return true;
+
+        if (Player.m_localPlayer)
+        {
+            Player.m_localPlayer.Message(MessageHud.MessageType.Center, "Cannot read trade route map again yet.");
+        }
+        return false;
     }
 
+    /// <summary>
+    ///     Coroutine to wait until UpdatPInTime has elapsed and
+    ///     then remove the PinData.
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator<object> RemovePinDataAfter(float seconds)
+    {
+        Log.LogInfo($"Will remove pin in {seconds} seconds");
+        yield return new WaitForSeconds(seconds);
+              
+        if (TraderPinData != null && Minimap.instance)
+        {
+            Minimap.instance.RemovePin(TraderPinData);
+        }
+        yield return null;
+    }
 }
