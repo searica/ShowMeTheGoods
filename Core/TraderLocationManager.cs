5e0433d6167c0b923f32d59ec4809fb3dd15c1ca
Searica <143636061+searica@users.noreply.github.com>
2025-01-03 08:56:44 -0800

    Remove debug line

--- Core/TraderLocationManager.cs
+++ Core/TraderLocationManager.cs
@@ -268,7 +268,6 @@ internal sealed class TraderLocationManager
             if (traderLocationInstances.ContainsKey(locationName))
             {
                 traderLocationInstances[locationName].Add(locationInstance);
-                Log.LogInfo($"Location Instance for {locationName} is placed: {locationInstance.m_placed}");
             }
         }
 
